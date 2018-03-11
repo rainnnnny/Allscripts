ed2ef34cb3972eeb359df4abc11434991aa4925f
rainnnnny <rainnnnny@sina.com>
2018-03-11 21:06:57 +0800

    tabs to spaces

--- log.py
+++ log.py
@@ -3,22 +3,22 @@ import os
 
 
 def getlogger(name):
-	logger = logging.getLogger(name)
+    logger = logging.getLogger(name)
 
-	if logger.hasHandlers():
-		return logger
+    if logger.hasHandlers():
+        return logger
 
-	formatter = logging.Formatter('%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s')
-	#"%(asctime)s %(name)s [%(levelname)s] %(thread)d %(module)s %(funcName)s %(lineno)s: %(message)s"
+    formatter = logging.Formatter('%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s')
+    #"%(asctime)s %(name)s [%(levelname)s] %(thread)d %(module)s %(funcName)s %(lineno)s: %(message)s"
         path = '~/var/log/%s.log'%name
         if not  os.path.isfile(path):
             f = open(path, 'w')
             f.close()
 
-    	handler = logging.FileHandler(path)
-	handler.formatter = formatter
-	
-	logger.addHandler(handler)
-	logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(path)
+    handler.formatter = formatter
+    
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
 
-	return logger
+    return logger
